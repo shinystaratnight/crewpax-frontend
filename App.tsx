45858a22d891f63d75bb73dc560cc4a52a2a168a
Rising Star <star-atnight@outlook.com>
2019-11-19 16:46:53 +0800

    Add plugin decorators settings and restructuring project

--- App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import AppNavigator from './src/routes/AppNavigator'
-
-interface Props { }
-const App: React.FC<Props> = () => {
-  return <AppNavigator />
-}
-
-export default App
--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React, { useEffect, useContext } from 'react'
-import { RootStoreContext } from '../stores/RootStore'
-import Welcome from '../screens/Welcome'
-import Home from '../screens/Home'
+import { RootStoreContext } from './stores/RootStore'
+import Welcome from './routes/Welcome'
+import Home from './routes/Home'
 
-export default function AppNavigator () {
-  
+const App = () => {
   const rootStore = useContext(RootStoreContext)
   
   useEffect(() => {
     
   }, [])
-  
+
   return (
     !rootStore.userStore.loggedIn ? 
       <Welcome /> : <Home />
   )
 }
+
+export default App
